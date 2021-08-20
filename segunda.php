d01489c0f51c8db0de4b4f193d691062cbd5b958
lndsilva <lndsilva@hotmail.com>
2021-08-19 22:13:37 -0300

    atualizando

---
+++ segunda.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<?php 
+	#comentário
+	#Data: 19/08/2021
+
+ ?>
+<head>
+	<meta charset="utf-8">
+	<title>PHP - Testando comentário</title>
+</head>
+<body>
+	<?php 
+
+		/*
+		Comentário de linha
+
+		*/
+		echo "Segundo exemplo de comentário";
+		$data = "19 de agosto de 2021";
+	 ?>
+	 <p>Este parágrafo está sendo interpretado pelo html</p>
+	 Arquivo criado em php <?php echo $data; ?>
+
+</body>
+</html>
